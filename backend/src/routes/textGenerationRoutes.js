60e7b34038f985fd081f1c5ffa1982b779acafa2
LeszczynskiKarol <leszczu123@wp.pl>
2025-10-21 22:45:54 +0200

    server fixc

--- backend/src/routes/textGenerationRoutes.js
+++ backend/src/routes/textGenerationRoutes.js
@@ -1,19 +1,19 @@
 // backend/src/routes/textGenerationRoutes.js
 const express = require('express');
 const router = express.Router();
-const { protect, adminProtect } = require('../middlewares/auth');
+const { protect, isAdmin } = require('../middlewares/authMiddleware'); // ZMIENIONE
 const textGenerationController = require('../controllers/textGenerationController');
 
 router.post(
   '/start',
   protect,
-  adminProtect,
+  isAdmin, // ZMIENIONE z adminProtect na isAdmin
   textGenerationController.startTextGeneration
 );
 router.post(
   '/batch-start',
   protect,
-  adminProtect,
+  isAdmin, // ZMIENIONE z adminProtect na isAdmin
   textGenerationController.batchStartTextGeneration
 );
 router.get(
@@ -24,7 +24,7 @@ router.get(
 router.get(
   '/search-results',
   protect,
-  adminProtect,
+  isAdmin, // ZMIENIONE z adminProtect na isAdmin
   textGenerationController.getAllSearchResults
 );
 router.get(
