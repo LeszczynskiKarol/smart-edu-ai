86fcf51f90430484b96fac8676372cb53a3bb85a
LeszczynskiKarol <leszczu123@wp.pl>
2025-10-27 10:14:29 +0100

    poprawki w robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,8 +5,8 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: ['/pl/', '/en/'],
-        disallow: ['/', '/examples/', '/api/', '/*?*', '/private/', '/admin/'],
+        allow: ['/', '/pl/', '/en/'],
+        disallow: ['/api/', '/*?*', '/private/', '/admin/'],
       },
     ],
     sitemap: 'https://www.smart-edu.ai/sitemap.xml',
